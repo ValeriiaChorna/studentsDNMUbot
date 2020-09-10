97f31915f42feec1b94c42357d4293e5ce2d8466
Valeri <valeria.chyornaya@gmail.com>
2020-09-11 02:24:58 +0300

    change url 2

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const options = {
     port: process.env.PORT,
   },
 };
-const url = process.env.APP_URL; //|| "https://<app-name>.herokuapp.com:443";
+const url = process.env.HEROKU_APP_NAME + `.herokuapp.com:443`;
+// const url = process.env.APP_URL; //|| "https://<app-name>.herokuapp.com:443";
 
 // const bot = new TelegramBot(TOKEN, {
 //   polling: true,
