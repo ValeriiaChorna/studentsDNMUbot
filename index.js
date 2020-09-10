e0a3bb3d35113765baa8cdc49b4008ab017d5043
Valeri <valeria.chyornaya@gmail.com>
2020-09-11 02:46:27 +0300

    change url 5

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const options = {
     port: process.env.PORT,
   },
 };
-const url = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com:443`;
+const url = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
 console.log("url:", url);
 // const url = process.env.APP_URL; //|| "https://<app-name>.herokuapp.com:443";
 
