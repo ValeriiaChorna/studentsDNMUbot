4202bb6011665156835dc1935ca02b2ad688f5ff
Valeri <valeria.chyornaya@gmail.com>
2020-09-16 05:09:20 +0300

    fix fun userModel3

--- users-model.js
+++ users-model.js
@@ -1,4 +1,5 @@
-const mongoose, { Schema } = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
 // const { ObjectId } = mongoose.Types;
 
 const usersSchema = new Schema({
